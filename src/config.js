9be0bff372d70768527b8d1e8a2931e45cc2e4a7
Heimdall <74313913+ichbinheimdall@users.noreply.github.com>
2021-10-27 23:19:49 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const config = { 
 //------------Connections-----------//
-    Client_Token: 'ODcwOTY3OTgyNTIyNzc3NjAx.YQUeJA.R5cioY2qntZRc9Fr14weg7jXqQs',
+    Client_Token: 'ODcwOTY3OTgyNTIyNzc3NjAx.YQUeJA.RN8XQP9yier_SxNEwBnBkmDNmSY',
     MongoDB_ConnectURL: 'mongodb+srv://Heimdall:o8vSAMidahunO4ys@cluster0.euijz.mongodb.net/database?retryWrites=true&w=majority',
     //------------BotSettings-----------//
     Prefix: '.',
     BotOwners: ["387675598044135436"],
-    Custom_Status_Text: 'hey, işleri yoluna koyunca hizmet vermeye devam edeceğim.',
+    Custom_Status_Text: ".davet | .yardım",
     Custom_Status_Type: 'PLAYING', // => PLAYING / WATCHING / LISTENING
     Custom_Status: 'idle' // => dnd / idle / online / invisible
 };
