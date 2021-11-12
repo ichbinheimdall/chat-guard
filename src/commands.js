d22b515b4ed37b5f05d9cbd92766113e4f6ef8d8
Heimdall <74313913+ichbinheimdall@users.noreply.github.com>
2021-11-12 18:50:13 +0300

    Update commands.js

--- src/commands.js
+++ src/commands.js
@@ -7,6 +7,39 @@ const moment = require("moment")
 require("moment-duration-format")
 moment.locale("tr")
 
+client.on('guildDelete', guild => {
+
+let rrrsembed = new Discord.RichEmbed()
+
+.setColor("RED")
+.setTitle(" Bot Kickledi ")
+.addField("Sunucu Adı:", guild.name)
+.addField("Sunucu sahibi", guild.owner)
+.addField("Sunucu Sahibi'nin ID'si", guild.ownerID)
+.addField("Sunucunun Kurulu Olduğu Bölge:", guild.region)
+.addField("Sunucudaki Kişi Sayısı:", guild.memberCount)
+
+   client.channels.get('898935076207870012').send(rrrsembed);
+  
+});
+
+
+client.on('guildCreate', guild => {
+
+let rrrsembed = new Discord.RichEmbed()
+
+.setColor("GREEN")
+.setTitle(" Bot Eklendi ")
+.addField("Sunucu Adı:", guild.name)
+.addField("Sunucu sahibi", guild.owner)
+.addField("Sunucu Sahibi'nin ID'si", guild.ownerID)
+.addField("Sunucunun Kurulu Olduğu Bölge:", guild.region)
+.addField("Sunucudaki Kişi Sayısı:", guild.memberCount)
+
+   client.channels.get('898935076207870012').send(rrrsembed);
+  
+});
+
 client.on('message', async message => {
     if(!message.guild || message.author.bot) return;
     if(!message.content.startsWith(config.Prefix)) return;
