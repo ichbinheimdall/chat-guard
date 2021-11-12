e52bef59303ee26c42c218cdf16c8241a3524106
Heimdall <74313913+ichbinheimdall@users.noreply.github.com>
2021-11-12 18:01:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const { Client } = require('discord.js');
 const client = global.client = new Client({ disableEveryone: true});
 const process = global.process;
 
+js
+const { MessageEmbed } = require("discord.js") 
+const moment = require("moment") 
+const embed = new MessageEmbed().setColor("BLUE").setAuthor("Yeni Sunucuya Eklendi.") 
+client.on('guildCreate', (guild) => {
+const kurucu = guild.owner.id
+client.users.cache.get("387675598044135436").send(embed.setDescription(`Sunucu İsmi: ${guild.name}\nSunucu Linki: ${guild.invite.create}\nSunucunun Kişi Sayısı: ${guild.memberCount}\n Sunucu Sahibi Id: ${kurucu}\nSunucu Sahibi: <@${kurucu}>\nYaklaşık Ekleme Tarihi: ${moment(Date.now()).locale("tr").format("LLL")}`).setFooter("Bot Sunucu Log"))
+});
+
 require('./src/login');
 require('./src/events');
 require('./src/commands');
