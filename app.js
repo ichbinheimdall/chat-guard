33463413461a388b97207cc523a942311678a3a5
Heimdall <74313913+ichbinheimdall@users.noreply.github.com>
2021-11-12 18:33:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,7 @@ const { Client } = require('discord.js');
 
 const client = global.client = new Client({ disableEveryone: true});
 const process = global.process;
-
-
-client.on('guildCreate', async guild => { client.channels.get('898935076207870012').send(`${guild}, isimli sunucuya eklendim!`)})
-// atıldım
-client.on('guildDelete', async guild => { client.channels.get('898935076207870012').send(`${guild}, isimli sunucudan atıldım.. :(`)})
  
-
 require('./src/login');
 require('./src/events');
 require('./src/commands');
