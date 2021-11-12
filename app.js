700a2d2a3615b946538270bc6dc885a3e4ef5f8e
Heimdall <74313913+ichbinheimdall@users.noreply.github.com>
2021-11-12 18:47:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,43 +3,7 @@ const Discord = require('discord.js');
 
 const client = global.client = new Client({ disableEveryone: true});
 const process = global.process;
- 
-client.on('guildDelete', guild => {
-
-let rrrsembed = new Discord.RichEmbed()
-
-.setColor("RED")
-.setTitle(" Bot Kickledi ")
-.addField("Sunucu Adı:", guild.name)
-.addField("Sunucu sahibi", guild.owner)
-.addField("Sunucu Sahibi'nin ID'si", guild.ownerID)
-.addField("Sunucunun Kurulu Olduğu Bölge:", guild.region)
-.addField("Sunucudaki Kişi Sayısı:", guild.memberCount)
-
-   client.channels.get('898935076207870012').send(rrrsembed);
-  
-});
-
-
-client.on('guildCreate', guild => {
-
-let rrrsembed = new Discord.RichEmbed()
-
-.setColor("GREEN")
-.setTitle(" Bot Eklendi ")
-.addField("Sunucu Adı:", guild.name)
-.addField("Sunucu sahibi", guild.owner)
-.addField("Sunucu Sahibi'nin ID'si", guild.ownerID)
-.addField("Sunucunun Kurulu Olduğu Bölge:", guild.region)
-.addField("Sunucudaki Kişi Sayısı:", guild.memberCount)
-
-   client.channels.get('898935076207870012').send(rrrsembed);
-  
-});
-
 
 require('./src/login');
 require('./src/events');
 require('./src/commands');
-
- 
