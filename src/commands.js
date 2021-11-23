faeaff4e27f055ec72ad4e29c497b29eeba9bfd8
Heimdall <74313913+ichbinheimdall@users.noreply.github.com>
2021-11-23 16:31:19 +0300

    Update commands.js

--- src/commands.js
+++ src/commands.js
@@ -18,7 +18,7 @@ client.on('message', async message => {
     if (command.toLowerCase() === "sunucu-ayrıl") {
         if(config.BotOwners.includes(message.member.id) === false ) return message.channel.send(`<a:red:909521226207203338> Bu komut yalnızca geliştiricime özeldir.`);
 
-      let guilds = client.guilds.cache.filter(g => g.memberCount < 10).array();
+      let guilds = client.guilds.cache.filter(g => g.memberCount < 15).array();
       for (let i = 0; i < guilds.length; i++) {
         setTimeout(() => guilds[i].leave().catch(() => {}), i * 5000);
       };
