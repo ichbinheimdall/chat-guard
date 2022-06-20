fb3840e9f3a45bc19e05df1339ab0c59ce64353f
Heimdall <74313913+ichbinheimdall@users.noreply.github.com>
2022-06-20 20:43:03 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const config = { 
 //------------Connections-----------//
-    Client_Token: 'ODcwOTY3OTgyNTIyNzc3NjAx.YQUeJA.0nPl0uvvzTieWDWWb1Xkvvu0lqQ',
+    Client_Token: 'ODcwOTY3OTgyNTIyNzc3NjAx.GDNK1x.Q8dXtR9tXTsXApOI1DGFiNbUua-xWYbY5hi7v0',
     MongoDB_ConnectURL: 'mongodb+srv://Heimdall:o8vSAMidahunO4ys@cluster0.euijz.mongodb.net/database?retryWrites=true&w=majority',
     //------------BotSettings-----------//
     Prefix: '.',
     BotOwners: ["697494349856112702"],
-    Custom_Status_Text: "despiadado.",
+    Custom_Status_Text: ".oy | .yardım",
     Custom_Status_Type: 'WATCHING', // => PLAYING / WATCHING / LISTENING
     Custom_Status: 'idle' // => dnd / idle / online / invisible
 };
