89a01f6e18f7ef52949e391660b6336314726e02
Heimdall <74313913+ichbinheimdall@users.noreply.github.com>
2022-07-23 18:01:31 +0300

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const { Client } = require('discord.js');
-const Discord = require('discord.js');
-
-const client = global.client = new Client({ disableEveryone: true});
-const process = global.process;
-
-require('./src/login');
-require('./src/events');
-require('./src/commands');
