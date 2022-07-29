be8323130836e665ca447567c134f4a8110ab74f
Heimdall <74313913+ichbinheimdall@users.noreply.github.com>
2022-07-29 17:16:11 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
         Client_Token: "ODcwOTY3OTgyNTIyNzc3NjAx.GtKpKv.SpPXYyRRfvmIAeH-EnfLzUIdseravwnxcaDbKY",
         MongoDB_ConnectURL: "mongodb+srv://Heimdall:o8vSAMidahunO4ys@cluster0.euijz.mongodb.net/database?retryWrites=true&w=majority",
         BotOwners: ["387675598044135436"],
-        BotStatus: "Chat Guard Yenilendi! Şimdi v14 ve slash komutlu! | /davet /yardım"
+        BotStatus: "Chat Guard Yenilendi! | /davet /yardım"
 };
